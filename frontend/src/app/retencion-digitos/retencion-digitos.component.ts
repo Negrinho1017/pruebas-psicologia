c44177cf1e20a6eadc5d34c2b158f97f1912834d
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2018-09-04 17:08:34 -0500

    RDD Vista completa

--- frontend/src/app/retencion-digitos/retencion-digitos.component.ts
+++ frontend/src/app/retencion-digitos/retencion-digitos.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RetencionDigitosComponent implements OnInit {
   selectedRetencionDeDigitos: number;
-  digitosRDD: String[] = ["9-7","6-3","5-8-2","6-9-4","7-2-8-6","6-4-3-9"];
+  digitosRDD: String[] = ["9 - 7","6 - 3","5 - 8 - 2","6 - 9 - 4","7 - 2 - 8 - 6","6 - 4 - 3 - 9"
+  ,"4 - 2 - 7 - 3 - 1","7 - 5 - 8 - 3 - 6","3 - 9 - 2 - 4 - 8 - 7",
+  "6 - 1 - 9 - 4 - 7 - 3","6 - 9 - 1 - 7 - 4 - 2 - 8","4 - 1 - 7 - 9 - 3 - 8 - 6"
+  ,"3 - 8 - 2 - 9 - 6 - 1 - 7 - 4","5 - 8 - 1 - 3 - 2 - 6 - 4 - 7","2 - 7 - 5 - 8 - 6 - 3 - 1 - 9 - 4",
+  "7 - 1 - 3 - 9 - 4 - 2 - 5 - 6 - 8"];
   numero: number;
 
   public pasarDeNumeroDeReactivo(i: number){
