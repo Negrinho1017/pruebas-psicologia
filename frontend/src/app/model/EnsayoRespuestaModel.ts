5d7d779aa826c4bee745858abca5db32d6179c2b
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2018-09-04 18:18:11 -0500

    RDD vista parcialmente terminada

---
+++ frontend/src/app/model/EnsayoRespuestaModel.ts
@@ -0,0 +1,8 @@
+export class EnsayoRespuestaModel {
+  public ensayo: String;
+  public respuesta: String;
+  constructor(ensayo, respuesta) {
+    this.ensayo = ensayo;
+    this.respuesta = respuesta;
+  }
+}
--- frontend/src/app/retencion-digitos/retencion-digitos.component.ts
+++ frontend/src/app/retencion-digitos/retencion-digitos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EnsayoRespuestaModel } from '../model/EnsayoRespuestaModel';
 
 @Component({
   selector: 'app-retencion-digitos',
@@ -12,6 +13,9 @@ export class RetencionDigitosComponent implements OnInit {
   "6 - 1 - 9 - 4 - 7 - 3","6 - 9 - 1 - 7 - 4 - 2 - 8","4 - 1 - 7 - 9 - 3 - 8 - 6"
   ,"3 - 8 - 2 - 9 - 6 - 1 - 7 - 4","5 - 8 - 1 - 3 - 2 - 6 - 4 - 7","2 - 7 - 5 - 8 - 6 - 3 - 1 - 9 - 4",
   "7 - 1 - 3 - 9 - 4 - 2 - 5 - 6 - 8"];
+  digitosRDI: EnsayoRespuestaModel[] = [new EnsayoRespuestaModel("3 - 1","1 - 3"),
+  new EnsayoRespuestaModel("2 - 4","4 - 2"),new EnsayoRespuestaModel("4 - 6","6 - 4"),
+  new EnsayoRespuestaModel("5 - 7","7 - 5")];
   numero: number;
 
   public pasarDeNumeroDeReactivo(i: number){
