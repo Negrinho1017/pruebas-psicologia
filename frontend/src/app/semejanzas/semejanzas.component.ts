efd1208b20b4c6e5098893315184afba22c7fbe8
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2018-09-08 12:13:59 -0500

    Añadido puntaje a semejanzas

--- frontend/src/app/semejanzas/semejanzas.component.ts
+++ frontend/src/app/semejanzas/semejanzas.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SemejanzasComponent implements OnInit {
   reactivos: String[] = ["M. Dos - Siete", "1. Tenedor - Cuchara", "2. Amarillo - Verde",
-  "3. Zanahoria - Brócoli", "4. Caballo - Tigre", "5. Piano - Tambor", "6. Barco - Automóvil", "7. Nariz - Lengua",
+  "3. Zanahoria - Brócoli", "*4. Caballo - Tigre", "*5. Piano - Tambor", "6. Barco - Automóvil", "7. Nariz - Lengua",
   "8. Comida - Gasolina", "9. Capullo - Bebé", "10. Ancla - Cerca", "11. Insignia - Corona", "12. Música - Marea",
   "13. Poema - Estatua", "14. Desear - Esperar", "15. Aceptación - Negación", "16. Siempre - Nunca",
   "17. Permitir - Restreingir","18. Enemigo - Amigo"];
-
+  puntuacion: number = 0;
   constructor() { }
 
   ngOnInit() {
