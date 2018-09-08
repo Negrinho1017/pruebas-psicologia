a08145a6561b77fc775378e687ad6ec3b860c4e6
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2018-09-08 13:00:35 -0500

    Añadido puntuación total para aritmética

--- frontend/src/app/aritmetica/aritmetica.component.ts
+++ frontend/src/app/aritmetica/aritmetica.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { EnsayoRespuestaModel } from '../model/EnsayoRespuestaModel';
+
 @Component({
   selector: 'app-aritmetica',
   templateUrl: './aritmetica.component.html',
   styleUrls: ['./aritmetica.component.css']
 })
 export class AritmeticaComponent implements OnInit {
-  respuestasCorrectas: String[] = ["3","Cuenta hasta 3","Cuenta hasta 10","6","9","2","8"];
+  respuestasCorrectas: EnsayoRespuestaModel[] = [new EnsayoRespuestaModel("*1. Flores","Cuenta hasta 3"),
+  new EnsayoRespuestaModel("*2. Manzanas","Cuenta hasta 10"),new EnsayoRespuestaModel("3. Bates","6"),
+  new EnsayoRespuestaModel("4. Pájaros","9"),new EnsayoRespuestaModel("5. Correas","2"),
+  new EnsayoRespuestaModel("6. Cobijas","8"),new EnsayoRespuestaModel("+17. Minutos","186")];
+  puntuacion: number = 0;
   constructor() { }
 
   ngOnInit() {
