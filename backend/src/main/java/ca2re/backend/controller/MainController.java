5f352ea6b8cb9bcd2db6e5e8d8abfe1c11510ca9
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2018-09-10 22:17:09 -0500

    Servicio para calcular edad

--- backend/src/main/java/ca2re/backend/controller/MainController.java
+++ backend/src/main/java/ca2re/backend/controller/MainController.java
@@ -1,10 +1,18 @@
 package ca2re.backend.controller;
 
+import java.text.ParseException;
+import java.util.Calendar;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import ca2re.backend.dominio.EdadPersona;
+import ca2re.backend.util.CalculadoraDeEdad;
+import ca2re.backend.util.FechaUtil;
 
 @RestController
 @RequestMapping(value = "/")
@@ -17,6 +25,15 @@ public class MainController {
 	@CrossOrigin
 	public String[] mostrarReactivosVocabulario() {
 		return reactivos;
+	}
+	
+	@RequestMapping(value = "/edad", method = RequestMethod.GET)
+	@ResponseBody
+	@CrossOrigin
+	public EdadPersona obtenerEdadPersona(@RequestParam String fechaNacimiento, @RequestParam String fechaEvaluacion) throws ParseException {
+		Calendar fechaNacimientoConvertida = FechaUtil.convertirDeStringACalendar(fechaNacimiento);
+		Calendar fechaEvaluacionConvertida = FechaUtil.convertirDeStringACalendar(fechaEvaluacion);
+		return CalculadoraDeEdad.calcularEdad(fechaEvaluacionConvertida, fechaNacimientoConvertida);
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
