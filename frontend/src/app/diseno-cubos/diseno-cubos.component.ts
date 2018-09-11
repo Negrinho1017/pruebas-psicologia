d56cefae16a679b4d25d6f211c2e20c053d76036
Andrés Carrasquilla <andresjulian.carrasquilla@gmail.com>
2018-09-10 20:54:56 -0500

    Diseño de cubos con imagenes
    Se agregan las imagenes a la vista de la subprueba de Diseño de cubos

--- frontend/src/app/diseno-cubos/diseno-cubos.component.ts
+++ frontend/src/app/diseno-cubos/diseno-cubos.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diseno-cubos.component.css']
 })
 export class DisenoCubosComponent implements OnInit {
-  imagenesCubos: any[] = ["Cubo1", "Cubo2", "Cubo3", "Cubo4", "Cubo5", "Hola", 
-  "", "","", "", "", "", "", "", ""];
+  imagenesCubos: any[] = ["CubosReactivo0.png", "CubosReactivo1.png", "CubosReactivo2.png", "CubosReactivo3.png", "CubosReactivo4.png", "CubosReactivo5.png", "CubosReactivo6.png", "CubosReactivo7.png", "CubosReactivo8.png", "CubosReactivo9.png", "CubosReactivo10.png", 
+  "CubosReactivo11.png", "CubosReactivo12.png", "CubosReactivo13.png", "CubosReactivo14.png"];
   constructor() { }
 
   ngOnInit() {
