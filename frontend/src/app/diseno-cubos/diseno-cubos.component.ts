bd46be90864ae2afdeb3ea7eb5ed98ca9dcd5400
Andrés Carrasquilla <andresjulian.carrasquilla@gmail.com>
2018-09-12 18:04:04 -0500

    Diseño cubos modificable

--- frontend/src/app/diseno-cubos/diseno-cubos.component.ts
+++ frontend/src/app/diseno-cubos/diseno-cubos.component.ts
@@ -6,11 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diseno-cubos.component.css']
 })
 export class DisenoCubosComponent implements OnInit {
-  imagenesCubos: any[] = ["CubosReactivo0.png", "CubosReactivo1.png", "CubosReactivo2.png", "CubosReactivo3.png", "CubosReactivo4.png", "CubosReactivo5.png", "CubosReactivo6.png", "CubosReactivo7.png", "CubosReactivo8.png", "CubosReactivo9.png", "CubosReactivo10.png", 
-  "CubosReactivo11.png", "CubosReactivo12.png", "CubosReactivo13.png", "CubosReactivo14.png"];
+
+  contador: number[][] = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0, 7, 0, 0],
+    [0, 0, 7, 0, 0],
+    [0, 0, 7, 0, 0],
+    [0, 0, 7, 0, 0],
+    [0, 0, 7, 0, 0],
+    [0, 0, 7, 0, 0],
+    [0, 0, 7, 0, 0],
+    [0, 0, 7, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+
+  imagenesCubos: any[] = ["CubosReactivo0.png", "CubosReactivo1.png", "CubosReactivo2.png", "CubosReactivo3.png", "CubosReactivo4.png", "CubosReactivo5.png", "CubosReactivo6.png", "CubosReactivo7.png", "CubosReactivo8.png", "CubosReactivo9.png", "CubosReactivo10.png",
+    "CubosReactivo11.png", "CubosReactivo12.png", "CubosReactivo13.png", "CubosReactivo14.png"];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  cambiarImg(i, posicion): void {
+    if (this.contador[i][posicion] == 5) {
+      this.contador[i][posicion] = 0;
+    }
+    else {
+      this.contador[i][posicion] = this.contador[i][posicion] + 1;
+    }
   }
 
 }
