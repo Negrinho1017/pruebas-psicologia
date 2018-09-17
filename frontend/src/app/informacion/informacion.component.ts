4194d3f51f50f8a671beee9f7c90e69ebb7e0e11
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2018-09-17 16:30:45 -0500

    Calificar información

--- frontend/src/app/informacion/informacion.component.ts
+++ frontend/src/app/informacion/informacion.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Reactivo } from '../model/Reactivo';
+import { Subprueba } from '../model/Subprueba';
+import { Globals } from '../globals';
+import { HojaDeResultadosService } from '../hoja-de-resultados/hoja-de-resultados.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-informacion',
@@ -6,11 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informacion.component.css']
 })
 export class InformacionComponent implements OnInit {
-  reactivos: String[] = ["*1. Lunes", "*2. Forma", "+3. Termómetro", "+4. Segundos", "5. Agua", "*6. Brasil"];
+  reactivos: String[] = ["*1. Lunes", "*2. Forma", "+3. Termómetro", "+4. Segundos", "5. Agua",
+  "*6. Brasil","7. Emiliano Zapata","8. Italia","9. El quijote de la mancha","10. Cleopatra","11. Sahara",
+  "12. Línea","13. Olimpiadas","14. Revolución mexicana","15. La malinche","16. Relatividad","17. Gandhi",
+  "18. Hervir","19. Órgano","20. Lengua","21. Catalina","*22. Vasos sanguíneos","23. Sherlock Holmes",
+  "*24. Minutos","25. Alicia","*26. Circunferencia"];
   puntuacion: number = 0;
-  constructor() { }
+  listaCalificaciones: number[] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  reactivosCalificados: Reactivo[] = [];
+  subprueba: Subprueba = new Subprueba();
+  reactivoActual: Reactivo;
+  hayDiscontinuacion: boolean = false;
+  constructor( private globals: Globals, private hojaDeResultadosService: HojaDeResultadosService,
+    private router: Router ) { }
 
   ngOnInit() {
+    this.subprueba.nombre = "Información";
+    this.subprueba.numeroSubprueba = 9;
+  }
+
+  calificarReactivo(puntuacionReactivo: number, numeroReactivo: number){
+    this.listaCalificaciones[numeroReactivo] = (puntuacionReactivo); 
+    this.calificarSubprueba();
+  }
+
+  calificarSubprueba(){
+    for (let calificacionReactivo of this.listaCalificaciones) {
+      this.puntuacion = this.puntuacion + calificacionReactivo;
+    } 
+    this.subprueba.puntuacionNatural=this.puntuacion;
+    this.crearReactivos();
+    this.puntuacion = 0; 
+}
+  crearReactivos(){
+    var i = 0;
+    for (let calificacionReactivo of this.listaCalificaciones) {
+      this.reactivoActual = new Reactivo();
+      this.reactivoActual.puntuacion=calificacionReactivo;
+      this.reactivosCalificados[i] = (this.reactivoActual);
+      i++;
+    }
+  }
+
+  finalizarSubprueba(){
+    this.subprueba.reactivos = this.reactivosCalificados;
+    this.hojaDeResultadosService.crearSubprueba(this.subprueba, this.globals.idEvaluado);
+    this.router.navigate(['/claves']);
   }
 
 }
