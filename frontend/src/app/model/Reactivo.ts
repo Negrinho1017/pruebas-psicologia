0d881f6f35d3ab7dd316cf563fa6ab53be547b75
Andrés Carrasquilla <andresjulian.carrasquilla@gmail.com>
2018-09-24 23:49:52 -0500

    Semejanzas guardando cada Reactivo en BD

--- frontend/src/app/model/Reactivo.ts
+++ frontend/src/app/model/Reactivo.ts
@@ -1,4 +1,4 @@
 export class Reactivo {
     puntuacion: number;
-    respuesta: String;
+    respuesta: String;    
 }
--- frontend/src/app/wais/semejanzas/semejanzas.component.ts
+++ frontend/src/app/wais/semejanzas/semejanzas.component.ts
@@ -12,59 +12,61 @@ import { PuntuacionEscalarService } from '../../puntuacion-escalar/puntuacion-es
   styleUrls: ['./semejanzas.component.css']
 })
 export class SemejanzasComponent implements OnInit {
-  siguienteReactivo = 4;  
+  siguienteReactivo = 4;
   reactivos: String[] = ["M. Dos - Siete", "1. Tenedor - Cuchara", "2. Amarillo - Verde",
-  "3. Zanahoria - Brócoli", "*4. Caballo - Tigre", "*5. Piano - Tambor", "6. Barco - Automóvil", "7. Nariz - Lengua",
-  "8. Comida - Gasolina", "9. Capullo - Bebé", "10. Ancla - Cerca", "11. Insignia - Corona", "12. Música - Marea",
-  "13. Poema - Estatua", "14. Desear - Esperar", "15. Aceptación - Negación", "16. Siempre - Nunca",
-  "17. Permitir - Restreingir","18. Enemigo - Amigo"];
+    "3. Zanahoria - Brócoli", "*4. Caballo - Tigre", "*5. Piano - Tambor", "6. Barco - Automóvil", "7. Nariz - Lengua",
+    "8. Comida - Gasolina", "9. Capullo - Bebé", "10. Ancla - Cerca", "11. Insignia - Corona", "12. Música - Marea",
+    "13. Poema - Estatua", "14. Desear - Esperar", "15. Aceptación - Negación", "16. Siempre - Nunca",
+    "17. Permitir - Restreingir", "18. Enemigo - Amigo"];
   puntuacion: number = 0;
   reactivosCalificados: Reactivo[] = [];
-  listaCalificaciones: number[] = [0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-  habilitaReactivo: boolean[] = [true, true, true, true, false, false, false, false, false, 
+  listaCalificaciones: number[] = [0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  habilitaReactivo: boolean[] = [true, true, true, true, false, false, false, false, false,
     false, false, false, false, false, false];
   subprueba: Subprueba = new Subprueba();
-  reactivoActual: Reactivo;  
-  constructor( private globals: Globals, private route: ActivatedRoute,
+  reactivoActual: Reactivo;
+  constructor(private globals: Globals, private route: ActivatedRoute,
     private hojaDeResultadosService: HojaDeResultadosService,
-    private router: Router, private puntuacionEscalarService: PuntuacionEscalarService ) { }
+    private router: Router, private puntuacionEscalarService: PuntuacionEscalarService) { }
 
 
-  calificarReactivo(puntuacionReactivo: number, numeroReactivo: number){
-    this.reactivoActual.respuesta = 
-      (document.getElementById("txtRespuesta"+numeroReactivo) as HTMLInputElement).value;
+  calificarReactivo(puntuacionReactivo: number, numeroReactivo: number) {
+    this.reactivoActual = new Reactivo();
+    this.reactivoActual.respuesta =
+      (document.getElementById("txtRespuesta" + numeroReactivo) as HTMLInputElement).value;
     this.reactivoActual.puntuacion = puntuacionReactivo;
-    this.listaCalificaciones[numeroReactivo] = (puntuacionReactivo); 
+    this.reactivosCalificados[numeroReactivo] = this.reactivoActual;
+    this.listaCalificaciones[numeroReactivo] = (puntuacionReactivo);
     this.aplicarInversion(puntuacionReactivo, numeroReactivo);
-    this.calificarSubprueba();    
+    this.calificarSubprueba();
   }
 
   aplicarInversion(puntuacionReactivo: number, numeroReactivo: number): void {
-    if(numeroReactivo == 5 && (puntuacionReactivo < 2 || this.listaCalificaciones[numeroReactivo-1] < 2)){
-      this.habilitaReactivo[numeroReactivo -2] = false;
+    if (numeroReactivo == 5 && (puntuacionReactivo < 2 || this.listaCalificaciones[numeroReactivo - 1] < 2)) {
+      this.habilitaReactivo[numeroReactivo - 2] = false;
       this.habilitaReactivo[numeroReactivo - 3] = false;
       this.listaCalificaciones[numeroReactivo - 2] = 0;
       this.listaCalificaciones[numeroReactivo - 3] = 0;
       this.siguienteReactivo = 3;
     }
-    else if( numeroReactivo == 2){
-      if(puntuacionReactivo < 2 || this.listaCalificaciones[numeroReactivo+1] < 2){
-        this.habilitaReactivo[numeroReactivo -1] = false;            
-        this.listaCalificaciones[numeroReactivo - 1] = 0; 
-        this.siguienteReactivo = numeroReactivo-1;
+    else if (numeroReactivo == 2) {
+      if (puntuacionReactivo < 2 || this.listaCalificaciones[numeroReactivo + 1] < 2) {
+        this.habilitaReactivo[numeroReactivo - 1] = false;
+        this.listaCalificaciones[numeroReactivo - 1] = 0;
+        this.siguienteReactivo = numeroReactivo - 1;
       } else {
         this.siguienteReactivo = 6;
       }
     }
-    else{
-      if(numeroReactivo == 3){
+    else {
+      if (numeroReactivo == 3) {
         this.siguienteReactivo = 2;
-      }   
-      else if(numeroReactivo == 1){
+      }
+      else if (numeroReactivo == 1) {
         this.siguienteReactivo = 6;
-      }         
+      }
       else {
-        this.siguienteReactivo = numeroReactivo+1;
+        this.siguienteReactivo = numeroReactivo + 1;
       }
     }
   }
@@ -73,41 +75,31 @@ export class SemejanzasComponent implements OnInit {
     return this.habilitaReactivo[i];
   }
 
-  calificarSubprueba(){
+  calificarSubprueba() {
     for (let calificacionReactivo of this.listaCalificaciones) {
       this.puntuacion = this.puntuacion + calificacionReactivo;
-    } 
-    this.subprueba.puntuacionNatural=this.puntuacion;
-    this.crearReactivos();
-    this.puntuacion = 0; 
-}
-  crearReactivos(){
-    var i = 0;
-    for (let calificacionReactivo of this.listaCalificaciones) {
-      this.reactivoActual = new Reactivo();
-      this.reactivoActual.puntuacion=calificacionReactivo;
-      this.reactivosCalificados[i] = (this.reactivoActual);
-      i++;
     }
-  }
-  
+    this.subprueba.puntuacionNatural = this.puntuacion;
+    this.puntuacion = 0;
+  }  
+
   ngOnInit() {
     this.subprueba.nombre = "Semejanzas";
     this.subprueba.numeroSubprueba = 2;
   }
 
-  finalizarSubprueba(){
+  finalizarSubprueba() {
     this.subprueba.reactivos = this.reactivosCalificados;
-    this.puntuacionEscalarService.obtenerPuntuacionEscalarSemejanzas("20:0-24:11",this.subprueba.puntuacionNatural)
-    .subscribe(res => {
-      this.subprueba.puntuacionEscalar = res;
-      this.hojaDeResultadosService.crearSubprueba(this.subprueba, this.globals.idEvaluado);
-      this.router.navigate(['/retencion-digitos']);
-    });
-    
+    this.puntuacionEscalarService.obtenerPuntuacionEscalarSemejanzas("20:0-24:11", this.subprueba.puntuacionNatural)
+      .subscribe(res => {
+        this.subprueba.puntuacionEscalar = res;
+        this.hojaDeResultadosService.crearSubprueba(this.subprueba, this.globals.idEvaluado);
+        this.router.navigate(['/retencion-digitos']);
+      });
+
   }
 
   getReactivoSiguiente(): number {
-    return this.siguienteReactivo;   
+    return this.siguienteReactivo;
   }
 }
