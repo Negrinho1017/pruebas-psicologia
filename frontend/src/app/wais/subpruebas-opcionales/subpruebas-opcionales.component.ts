163675132b4adbb257c6cf395aa18ff287d7c42b
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2018-10-09 13:08:34 -0500

    Datalist

--- frontend/src/app/wais/subpruebas-opcionales/subpruebas-opcionales.component.ts
+++ frontend/src/app/wais/subpruebas-opcionales/subpruebas-opcionales.component.ts
@@ -9,8 +9,14 @@ export class SubpruebasOpcionalesComponent implements OnInit {
   listaSubpruebas: String[] = ["Diseños de cubos", "Semejanzas","Retención de dígitos","Matrices","Vocabulario",
   "Aritmética","Búsqueda de símbolos","Rompecabezas visual","Información","Claves","Sucesión de números y letras",
   "Peso figurado","Comprensión","Cancelación","Figuras incompletas"];
+  pruebasAEvaluar: boolean[] = [true,true,true,true,true,true,true,true,true,true,false,false,false,false,false];
   constructor() { }
+  
   ngOnInit() {
   }
 
+  metodoPrueba(i: number){
+    let variable = <HTMLInputElement>document.getElementById("pruebacheck"+i);
+    this.pruebasAEvaluar[i] = variable.checked;
+  }
 }
