a8124df7f61c24b524ed3496ce4fc1047887246b
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2018-11-29 01:13:28 -0500

    Refactor 5
    - Implementación de las entidades de valor crítico
    - Implementación correcta del builder de valor crítico

--- backend/src/main/java/ca2re/backend/persistencia/builder/ValorCriticoBuilder.java
+++ backend/src/main/java/ca2re/backend/persistencia/builder/ValorCriticoBuilder.java
@@ -6,18 +6,25 @@ import ca2re.backend.persistencia.mongo.entidades.EntidadValorCriticoWAIS;
 public class ValorCriticoBuilder {
 	public static EntidadValorCriticoWAIS convertirAEntidad(ValorCriticoWAIS valorCriticoWAIS) {
 		EntidadValorCriticoWAIS entidadValorCriticoWAIS = new EntidadValorCriticoWAIS();
-		valorCriticoWAIS.setIdRangoEdad(valorCriticoWAIS.getIdRangoEdad());
-		valorCriticoWAIS.setValorCriticoICV_IMT(valorCriticoWAIS.getValorCriticoICV_IMT());
-		valorCriticoWAIS.setValorCriticoICV_IRP(valorCriticoWAIS.getValorCriticoICV_IRP());
-		valorCriticoWAIS.setValorCriticoICV_IVP(valorCriticoWAIS.getValorCriticoICV_IVP());
-		valorCriticoWAIS.setValorCriticoIMT_IVP(valorCriticoWAIS.getValorCriticoIMT_IVP());
-		valorCriticoWAIS.setValorCriticoIRP_IMT(valorCriticoWAIS.getValorCriticoIRP_IMT());
-		valorCriticoWAIS.setValorCriticoIRP_IVP(valorCriticoWAIS.getValorCriticoIRP_IVP());
+		entidadValorCriticoWAIS.setIdRangoEdad(valorCriticoWAIS.getIdRangoEdad());
+		entidadValorCriticoWAIS.setValorCriticoICV_IMT(valorCriticoWAIS.getValorCriticoICV_IMT());
+		entidadValorCriticoWAIS.setValorCriticoICV_IRP(valorCriticoWAIS.getValorCriticoICV_IRP());
+		entidadValorCriticoWAIS.setValorCriticoICV_IVP(valorCriticoWAIS.getValorCriticoICV_IVP());
+		entidadValorCriticoWAIS.setValorCriticoIMT_IVP(valorCriticoWAIS.getValorCriticoIMT_IVP());
+		entidadValorCriticoWAIS.setValorCriticoIRP_IMT(valorCriticoWAIS.getValorCriticoIRP_IMT());
+		entidadValorCriticoWAIS.setValorCriticoIRP_IVP(valorCriticoWAIS.getValorCriticoIRP_IVP());
 		return entidadValorCriticoWAIS;
 	}
 	
 	public static ValorCriticoWAIS convertirADominio(EntidadValorCriticoWAIS entidadValorCriticoWAIS) {
 		ValorCriticoWAIS valorCriticoWAIS = new ValorCriticoWAIS();
+		valorCriticoWAIS.setIdRangoEdad(entidadValorCriticoWAIS.getIdRangoEdad());
+		valorCriticoWAIS.setValorCriticoICV_IMT(entidadValorCriticoWAIS.getValorCriticoICV_IMT());
+		valorCriticoWAIS.setValorCriticoICV_IRP(entidadValorCriticoWAIS.getValorCriticoICV_IRP());
+		valorCriticoWAIS.setValorCriticoICV_IVP(entidadValorCriticoWAIS.getValorCriticoICV_IVP());
+		valorCriticoWAIS.setValorCriticoIMT_IVP(entidadValorCriticoWAIS.getValorCriticoIMT_IVP());
+		valorCriticoWAIS.setValorCriticoIRP_IMT(entidadValorCriticoWAIS.getValorCriticoIRP_IMT());
+		valorCriticoWAIS.setValorCriticoIRP_IVP(entidadValorCriticoWAIS.getValorCriticoIRP_IVP());
 		return valorCriticoWAIS;
 	}
 }
--- backend/src/main/java/ca2re/backend/persistencia/mongo/CalificacionValoresCriticosMongoDAO.java
+++ backend/src/main/java/ca2re/backend/persistencia/mongo/CalificacionValoresCriticosMongoDAO.java
@@ -9,6 +9,8 @@ import org.springframework.data.mongodb.core.query.Query;
 
 import ca2re.backend.dominio.ValorCriticoWAIS;
 import ca2re.backend.persistencia.CalificacionValoresCriticosDAO;
+import ca2re.backend.persistencia.builder.ValorCriticoBuilder;
+import ca2re.backend.persistencia.mongo.entidades.EntidadValorCriticoWAIS;
 
 public class CalificacionValoresCriticosMongoDAO implements CalificacionValoresCriticosDAO{
 
@@ -25,8 +27,9 @@ public class CalificacionValoresCriticosMongoDAO implements CalificacionValoresC
 	@Override
 	public ValorCriticoWAIS obtenerValoresCriticos(int idRangoEdad) {
 		Query valoresCriticos = query(where("idRangoEdad").is(idRangoEdad));
-		return mongoOperations
-				.find(valoresCriticos, ValorCriticoWAIS.class, COLLECTION_VALORES_CRITICOS).get(0);
+		EntidadValorCriticoWAIS entidadValorCriticoWAIS = mongoOperations
+				.find(valoresCriticos, EntidadValorCriticoWAIS.class, COLLECTION_VALORES_CRITICOS).get(0);
+		return ValorCriticoBuilder.convertirADominio(entidadValorCriticoWAIS);
 	}
 
 }
