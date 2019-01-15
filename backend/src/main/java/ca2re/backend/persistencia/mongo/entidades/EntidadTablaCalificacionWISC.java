06e0ada4690a175cb1606c5b5948d3fa00321798
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2019-01-15 17:18:00 -0500

    Tabla de calificaciones WISC

---
+++ backend/src/main/java/ca2re/backend/persistencia/mongo/entidades/EntidadTablaCalificacionWISC.java
@@ -0,0 +1,148 @@
+package ca2re.backend.persistencia.mongo.entidades;
+
+public class EntidadTablaCalificacionWISC {
+	private String idEdad;
+	private String[] disenioCubosDC;
+	private String[] semejanzasSE;
+	private String[] digitosRD;
+	private String[] conceptosDibujosCD;
+	private String[] clavesCL;
+	private String[] vocabularioVB;
+	private String[] susecionNumeroLetrasNL;
+	private String[] matricesMT;
+	private String[] comprensionCM;
+	private String[] busquedaSimbolosBS;
+	private String[] figurasIncompletasFI;
+	private String[] registrosRG;
+	private String[] informacionIN;
+	private String[] aritmeticaAR;
+	private String[] pistasPC;
+
+	public String getIdEdad() {
+		return idEdad;
+	}
+
+	public void setIdEdad(String idEdad) {
+		this.idEdad = idEdad;
+	}
+
+	public String[] getDisenioCubosDC() {
+		return disenioCubosDC;
+	}
+
+	public void setDisenioCubosDC(String[] disenioCubosDC) {
+		this.disenioCubosDC = disenioCubosDC;
+	}
+
+	public String[] getSemejanzasSE() {
+		return semejanzasSE;
+	}
+
+	public void setSemejanzasSE(String[] semejanzasSE) {
+		this.semejanzasSE = semejanzasSE;
+	}
+
+	public String[] getDigitosRD() {
+		return digitosRD;
+	}
+
+	public void setDigitosRD(String[] digitosRD) {
+		this.digitosRD = digitosRD;
+	}
+
+	public String[] getConceptosDibujosCD() {
+		return conceptosDibujosCD;
+	}
+
+	public void setConceptosDibujosCD(String[] conceptosDibujosCD) {
+		this.conceptosDibujosCD = conceptosDibujosCD;
+	}
+
+	public String[] getClavesCL() {
+		return clavesCL;
+	}
+
+	public void setClavesCL(String[] clavesCL) {
+		this.clavesCL = clavesCL;
+	}
+
+	public String[] getVocabularioVB() {
+		return vocabularioVB;
+	}
+
+	public void setVocabularioVB(String[] vocabularioVB) {
+		this.vocabularioVB = vocabularioVB;
+	}
+
+	public String[] getSusecionNumeroLetrasNL() {
+		return susecionNumeroLetrasNL;
+	}
+
+	public void setSusecionNumeroLetrasNL(String[] susecionNumeroLetrasNL) {
+		this.susecionNumeroLetrasNL = susecionNumeroLetrasNL;
+	}
+
+	public String[] getMatricesMT() {
+		return matricesMT;
+	}
+
+	public void setMatricesMT(String[] matricesMT) {
+		this.matricesMT = matricesMT;
+	}
+
+	public String[] getComprensionCM() {
+		return comprensionCM;
+	}
+
+	public void setComprensionCM(String[] comprensionCM) {
+		this.comprensionCM = comprensionCM;
+	}
+
+	public String[] getBusquedaSimbolosBS() {
+		return busquedaSimbolosBS;
+	}
+
+	public void setBusquedaSimbolosBS(String[] busquedaSimbolosBS) {
+		this.busquedaSimbolosBS = busquedaSimbolosBS;
+	}
+
+	public String[] getFigurasIncompletasFI() {
+		return figurasIncompletasFI;
+	}
+
+	public void setFigurasIncompletasFI(String[] figurasIncompletasFI) {
+		this.figurasIncompletasFI = figurasIncompletasFI;
+	}
+
+	public String[] getRegistrosRG() {
+		return registrosRG;
+	}
+
+	public void setRegistrosRG(String[] registrosRG) {
+		this.registrosRG = registrosRG;
+	}
+
+	public String[] getInformacionIN() {
+		return informacionIN;
+	}
+
+	public void setInformacionIN(String[] informacionIN) {
+		this.informacionIN = informacionIN;
+	}
+
+	public String[] getAritmeticaAR() {
+		return aritmeticaAR;
+	}
+
+	public void setAritmeticaAR(String[] aritmeticaAR) {
+		this.aritmeticaAR = aritmeticaAR;
+	}
+
+	public String[] getPistasPC() {
+		return pistasPC;
+	}
+
+	public void setPistasPC(String[] pistasPC) {
+		this.pistasPC = pistasPC;
+	}
+}
