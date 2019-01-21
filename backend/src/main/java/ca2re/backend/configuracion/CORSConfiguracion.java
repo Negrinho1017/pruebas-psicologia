32913cc2ee0437ecf6cf56a0615d0f9baefd2975
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2019-01-21 10:55:55 -0500

    Cambios en el backend

--- backend/src/main/java/ca2re/backend/configuracion/CORSConfiguracion.java
+++ backend/src/main/java/ca2re/backend/configuracion/CORSConfiguracion.java
@@ -14,7 +14,7 @@ public class CORSConfiguracion {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://localhost:4200");
+		config.addAllowedOrigin("http://localhost");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("GET");
 		config.addAllowedMethod("PUT");
