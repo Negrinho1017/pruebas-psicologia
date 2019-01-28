d65dddc1296a384661937f210ac6656a5e951542
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2019-01-28 16:48:22 -0500

    Corrección de daños

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,44 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { Globals } from './globals';
 import { Router } from '@angular/router';
+import { PlatformLocation } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit  {
   title = 'wais';
-  idPrueba = this.globals.idPrueba; 
+  idPrueba = this.globals.idPrueba;
   mostrarNavBar = this.globals.mostrarNavBar;
 
-  constructor(private globals: Globals) { }
+  constructor(location: PlatformLocation, private globals: Globals, private router: Router) { 
+    location.onPopState(() => {
+      this.mensajeError("No puedes volver atrás");
+      this.router.navigateByUrl('/diseno-cubos');
+      history.forward();
+      });
+  }
 
-  salir(){
+  ngOnInit() {
+    window.addEventListener("beforeunload", function (e) {
+      var confirmationMessage = "\o/";
+      e.returnValue = confirmationMessage;
+      return confirmationMessage;     
+    });
+  }
+
+  salir() {
     localStorage.removeItem('idEvaluado');
     this.globals.mostrarNavBar = false;
+  }
+
+  mensajeError(mensaje: string) {
+    swal({
+      title: 'Error',
+      icon: "error",
+      text: mensaje,
+    });
   }
 }
