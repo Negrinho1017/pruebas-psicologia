0b25886268a1a8ee4022a8a135b5b2c73c9eb0d9
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2019-02-03 15:17:50 -0500

    Mostrar y redireccionar en hoja de subpruebas a las pruebas wisc

--- frontend/src/app/hoja-de-subpruebas/hoja-de-subpruebas.component.ts
+++ frontend/src/app/hoja-de-subpruebas/hoja-de-subpruebas.component.ts
@@ -16,12 +16,14 @@ export class HojaDeSubpruebasComponent implements OnInit {
   nombreSubpruebasWISC: String[] = ["Diseño de cubos","Semejanzas","Retención de dígitos",
   "Conceptos con dibujos","Claves","Vocabulario","Sucesión de números y letras","Matrices","Comprensión",
   "Búsqueda de símbolos","Figuras incompletas","Registros","Información","Aritmética","Pistas"];
+  nombresSubpruebas: String[];
   numerosSubprueba: number[] = [];
   constructor( private hojaDeSubpruebasService : HojaDeSubpruebasService, private globals: Globals,
     private router: Router) { }
 
   ngOnInit() {
     this.tipoPrueba = localStorage.getItem('tipoPrueba');
+    this.nombresSubpruebas = this.tipoPrueba == "WAIS" ? this.nombreSubpruebasWAIS : this.nombreSubpruebasWISC;
     this.hojaDeSubpruebasService.obtenerPruebaPorIdDelEvaluado(<string> this.globals.idEvaluado).subscribe(
       res => {
         this.numerosSubprueba = [res.ramaDelConocimiento[0].subpruebas[0].numeroSubprueba, res.ramaDelConocimiento[0].subpruebas[1].numeroSubprueba,
@@ -37,6 +39,48 @@ export class HojaDeSubpruebasComponent implements OnInit {
   }
 
   redireccionar(numeroSubprueba: number){
+    if(this.tipoPrueba == "WAIS"){
+      this.redireccionarWAIS(numeroSubprueba);
+    }else{
+      this.redireccionarWISC(numeroSubprueba);
+    }
+  }
+
+  redireccionarWISC(numeroSubprueba: number){
+    if(numeroSubprueba == 1){
+      this.router.navigate(['/diseno-cubos-wisc'])
+    }else if(numeroSubprueba == 2){
+      this.router.navigate(['/semejanzas-wisc'])
+    }else if(numeroSubprueba == 3){
+      this.router.navigate(['/retencion-digitos-wisc'])
+    }else if(numeroSubprueba == 4){
+      this.router.navigate(['/conceptos-con-dibujos'])
+    }else if(numeroSubprueba == 5){
+      this.router.navigate(['/claves-wisc'])
+    }else if(numeroSubprueba == 6){
+      this.router.navigate(['/vocabulario-wisc'])
+    }else if(numeroSubprueba == 7){
+      this.router.navigate(['/numeros-letras-wisc'])
+    }else if(numeroSubprueba == 8){
+      this.router.navigate(['/matrices-wisc'])
+    }else if(numeroSubprueba == 9){
+      this.router.navigate(['/comprension-wisc'])
+    }else if(numeroSubprueba == 10){
+      this.router.navigate(['/busqueda-simbolos-wisc'])
+    }else if(numeroSubprueba == 11){
+      this.router.navigate(['/figuras-incompletas-wisc'])
+    }else if(numeroSubprueba == 12){
+      this.router.navigate(['/registros'])
+    }else if(numeroSubprueba == 13){
+      this.router.navigate(['/informacion-wisc'])
+    }else if(numeroSubprueba == 14){
+      this.router.navigate(['/aritmetica-wisc'])
+    }else{
+      this.router.navigate(['/pistas'])
+    }
+  }
+
+  redireccionarWAIS(numeroSubprueba: number){
     if(numeroSubprueba == 1){
       this.router.navigate(['/diseno-cubos'])
     }else if(numeroSubprueba == 2){
